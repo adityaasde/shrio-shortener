7a7db67371b287941f58512d637f23bde645e50e
adityaa-swe <adityaa.sde@gmail.com>
2025-10-09 23:38:43 +0530

    add loader component

---
+++ shrio/src/components/BtnLoader.tsx
@@ -0,0 +1,7 @@
+export default function BtnLoader() {
+    return (
+        <div className="p-0.5">
+            <div id="loader" className="border-black border-2 w-5 h-5 rounded-full mx-auto border-t-transparent animate-spin"/>
+        </div>
+    )
+}
