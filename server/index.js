389b19f2324a1e586ff6782a76d2f1e7d2aedcee
adityaa-swe <adityaa.sde@gmail.com>
2025-10-11 17:34:07 +0530

    update with ratelimit and dep

--- server/index.js
+++ server/index.js
@@ -11,6 +11,8 @@ import UserRoutes from "./routes/UserRoutes.js";
 import cookieParser from "cookie-parser";
 import { redirectTo } from "./controllers/LinkController.js";
 import useragent from "express-useragent";
+import { redirectForQr } from "./controllers/QrController.js";
+import { rateLimit } from 'express-rate-limit'
 
 const app = express();
 dotenv.config({
@@ -19,10 +21,25 @@ dotenv.config({
 
 app.use(
   cors({
+    origin: `${process.env.BASE_URL}`,
     quiet: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   })
 );
-app.use(morgan("tiny"));
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 50,
+  standardHeaders: "disabled",
+  legacyHeaders: false,
+  ipv6Subnet: 56,
+  message: "too many request, try again later.",
+});
+
+app.use(limiter);
+app.use(morgan("short"));
 app.use(compression());
 app.use(express.json({ limit: "10mb" }));
 app.use(helemt());
@@ -35,11 +52,12 @@ app.use("/link", LinkRoutes);
 app.use("/qr", QrRoutes);
 app.use("/user", UserRoutes);
 app.get("/:slug", redirectTo);
+app.get("/scan/:slug", redirectForQr);
 
 const PORT = 9500;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running`);
 });
 
 app.get("/", (req, res) => {
